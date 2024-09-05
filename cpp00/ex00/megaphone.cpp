580e643041b5c4825fac12ae7dc1396a44f5613d
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-09-05 19:15:28 +0200

    [00/00] rec: Record wip version with all character support

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,28 +1,18 @@
-#include <cctype>
 #include <iostream>
+#include <locale>
 #include <string>
 
-void print_uppercase(std::string str)
+int main()
 {
-    std::string::iterator it;
+    std::locale::global(std::locale(""));
+    std::wcout.imbue(std::locale());
+    const std::ctype<wchar_t>& facet = std::use_facet<std::ctype<wchar_t> >(std::locale());
 
-    for (it = str.begin(); it != str.end(); it++)
-        std::cout << static_cast<char>(toupper(static_cast<unsigned char>(*it)));
-}
+    std::wstring str = L"äëñaóűßδ*.";
 
-int main(int argc, char* argv[])
-{
-    if (argc == 1)
-	{
-        print_uppercase("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
-	}
-    else
-	{
-		for (int i = 1; argv[i]; i++)
-		{
-			print_uppercase(argv[i]);
-			std::cout << " ";
-		}
-	}
-    std::cout << std::endl;
+    facet.toupper(&str[0], &str[0] + str.size());
+
+    std::wcout << str << std::endl;
+
+    return 0;
 }
