6fea6e23651343bf8825f8b6aa226a66fdceb876
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-09-30 10:42:04 +0200

    [00/00] style: Change indentation to tabs
    I will now use tabs for indentation and spaces for alignment.

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -14,49 +14,49 @@ static void strToUpper(std::wstring &str);
 
 int main(int argc, char *argv[])
 {
-    std::wstring str = L"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::wstring str = L"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
-    std::setlocale(LC_CTYPE, "");
-    if (argc > 1)
-    {
-        str = concatStrArr(&argv[1]);
-        strToUpper(str);
-    }
-    std::wcout << str << std::endl;
-    return 0;
+	std::setlocale(LC_CTYPE, "");
+	if (argc > 1)
+	{
+		str = concatStrArr(&argv[1]);
+		strToUpper(str);
+	}
+	std::wcout << str << std::endl;
+	return 0;
 }
 
 static std::wstring concatStrArr(char *arr[])
 {
-    std::wstring str;
+	std::wstring str;
 
-    for (int i = 0; arr[i]; ++i)
-    {
-        str.append(convertToWstring(arr[i]));
-    }
-    return str;
+	for (int i = 0; arr[i]; ++i)
+	{
+		str.append(convertToWstring(arr[i]));
+	}
+	return str;
 }
 
 static std::wstring convertToWstring(const char *str)
 {
-    size_t len;
-    std::wstring wstr;
+	size_t len;
+	std::wstring wstr;
 
-    len = mbslen(str);
-    wstr.resize(len);
-    std::mbstowcs(&wstr.at(0), str, len);
-    return wstr;
+	len = mbslen(str);
+	wstr.resize(len);
+	std::mbstowcs(&wstr.at(0), str, len);
+	return wstr;
 }
 
 static size_t mbslen(const char *str)
 {
-    return std::mbstowcs(NULL, str, 0);
+	return std::mbstowcs(NULL, str, 0);
 }
 
 static void strToUpper(std::wstring &str)
 {
-    for (std::wstring::iterator it = str.begin(); it != str.end(); ++it)
-    {
-        *it = std::towupper(*it);
-    }
+	for (std::wstring::iterator it = str.begin(); it != str.end(); ++it)
+	{
+		*it = std::towupper(*it);
+	}
 }
