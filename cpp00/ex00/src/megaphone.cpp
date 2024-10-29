813dd700b5a045f390167bbbe0510e0f0d354511
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-10-29 14:13:30 +0100

    [00/00] style: Change to snake_case

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -10,10 +10,10 @@
  * wcs = wide char string
  */
 
-static std::wstring concatStrArr(char *arr[]);
-static std::wstring convertToWstring(const char *mbs);
+static std::wstring concat_str_arr(char *arr[]);
+static std::wstring convert_to_wstring(const char *mbs);
 static size_t wcslen(const char *mbs);
-static void strToUpper(std::wstring &wcs);
+static void str_toupper(std::wstring &wcs);
 
 int main(int argc, char *argv[])
 {
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 
 	std::setlocale(LC_CTYPE, "");
 	if (argc > 1) {
-		wcs = concatStrArr(&argv[1]);
-		strToUpper(wcs);
+		wcs = concat_str_arr(&argv[1]);
+		str_toupper(wcs);
 	}
 	if (wcs.length()) {
 		std::wcout << wcs << std::endl;
@@ -30,17 +30,17 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-static std::wstring concatStrArr(char *arr[])
+static std::wstring concat_str_arr(char *arr[])
 {
 	std::wstring wcs;
 
 	for (int i = 0; arr[i]; ++i) {
-		wcs.append(convertToWstring(arr[i]));
+		wcs.append(convert_to_wstring(arr[i]));
 	}
 	return wcs;
 }
 
-static std::wstring convertToWstring(const char *mbs)
+static std::wstring convert_to_wstring(const char *mbs)
 {
 	size_t len;
 	std::wstring wcs;
@@ -58,7 +58,7 @@ static size_t wcslen(const char *mbs)
 	return std::mbstowcs(NULL, mbs, 0);
 }
 
-static void strToUpper(std::wstring &wcs)
+static void str_toupper(std::wstring &wcs)
 {
 	for (std::wstring::iterator it = wcs.begin(); it != wcs.end(); ++it) {
 		*it = std::towupper(*it);
