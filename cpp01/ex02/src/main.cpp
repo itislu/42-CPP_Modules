ae249df5d23f2bb5de1467f163469a780b4edccb
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-11-03 16:27:47 +0100

    [01/02] feat: Print comparison between pointers and references

---
+++ cpp01/ex02/src/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string string("HI THIS IS BRAIN");
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "ADDRESSES:" << "\n";
+	std::cout << &string << "\n";
+	std::cout << stringPTR << "\n";
+	std::cout << &stringREF << "\n\n";
+
+	std::cout << "VALUES:" << "\n";
+	std::cout << string << "\n";
+	std::cout << *stringPTR << "\n";
+	std::cout << stringREF << "\n";
+}
