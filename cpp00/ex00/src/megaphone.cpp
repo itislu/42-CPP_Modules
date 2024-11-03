1bc7a7e9259bb331f72bc636948990abe615553f
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-11-03 23:24:43 +0100

    [00/00] improve: Apply suggestions by clang-tidy

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -1,3 +1,6 @@
+// NOLINTBEGIN(misc-use-anonymous-namespace)
+// NOLINTBEGIN(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+
 #include <clocale>
 #include <cstdlib>
 #include <cwchar>
@@ -24,8 +27,8 @@ int main(int argc, char* argv[])
 		wcs = concat_str_arr(&argv[1]);
 		str_toupper(wcs);
 	}
-	if (wcs.length()) {
-		std::wcout << wcs << std::endl;
+	if (!wcs.empty()) {
+		std::wcout << wcs << '\n';
 	}
 	return 0;
 }
@@ -34,7 +37,7 @@ static std::wstring concat_str_arr(char* arr[])
 {
 	std::wstring wcs;
 
-	for (int i = 0; arr[i]; ++i) {
+	for (int i = 0; arr[i] != NULL; ++i) {
 		wcs.append(convert_to_wstring(arr[i]));
 	}
 	return wcs;
@@ -42,11 +45,10 @@ static std::wstring concat_str_arr(char* arr[])
 
 static std::wstring convert_to_wstring(const char* mbs)
 {
-	size_t len;
+	size_t len = wcslen(mbs);
 	std::wstring wcs;
 
-	len = wcslen(mbs);
-	if (len) {
+	if (len != 0) {
 		wcs.resize(len);
 		mbstowcs(&wcs.at(0), mbs, len);
 	}
@@ -61,6 +63,9 @@ static size_t wcslen(const char* mbs)
 static void str_toupper(std::wstring& wcs)
 {
 	for (std::wstring::iterator it = wcs.begin(); it != wcs.end(); ++it) {
-		*it = towupper(*it);
+		*it = (wchar_t)towupper(*it);
 	}
 }
+
+// NOLINTEND(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+// NOLINTEND(misc-use-anonymous-namespace)
