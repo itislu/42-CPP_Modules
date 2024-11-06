03a39eadd17653703c74d2a203cf284110b783d0
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-11-06 01:09:31 +0100

    [01/04] feat: Implement search and replace for a file

---
+++ cpp01/ex04/src/main.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+enum error {
+	ERROR_USAGE = 1,
+	ERROR_INFILE = 2,
+	ERROR_OUTFILE = 3
+};
+
+int main(int argc, char* argv[])
+{
+	if (argc != 4) {
+		std::cerr << "Usage: ./sed <filename> <search> <replace>" << '\n';
+		return ERROR_USAGE;
+	}
+	std::string infile_name(*(argv + 1));
+	std::string search(*(argv + 2));
+	std::string replace(*(argv + 3));
+	std::string outfile_name(infile_name + ".replace");
+
+	std::ifstream infile(infile_name.c_str(), std::ifstream::in);
+	if (!infile) {
+		perror(infile_name.c_str());
+		return ERROR_INFILE;
+	}
+
+	std::ofstream outfile(outfile_name.c_str(),
+						  std::ofstream::out | std::ofstream::trunc);
+	if (!outfile) {
+		perror(outfile_name.c_str());
+		return ERROR_OUTFILE;
+	}
+
+	std::stringstream buffer;
+	buffer << infile.rdbuf();
+	if (infile.fail()) {
+		perror(infile_name.c_str());
+		return ERROR_INFILE;
+	}
+
+	std::string content(buffer.str());
+	if (!search.empty()) {
+		size_t start = 0;
+		while ((start = content.find(search, start)) != std::string::npos) {
+			content.erase(start, search.length());
+			content.insert(start, replace);
+			start += replace.length();
+		}
+	}
+
+	outfile << content;
+	if (outfile.fail()) {
+		perror(outfile_name.c_str());
+		return ERROR_OUTFILE;
+	}
+}
