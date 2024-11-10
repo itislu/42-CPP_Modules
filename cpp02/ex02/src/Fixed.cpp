b549acd8427e87c42e48f2ba89450ea5e102209a
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-11-10 11:10:37 +0100

    [02/02] chore: Remove constructor/destructor msgs

--- cpp02/ex02/src/Fixed.cpp
+++ cpp02/ex02/src/Fixed.cpp
@@ -2,35 +2,21 @@
 #include <cmath>
 #include <iostream>
 
-Fixed::Fixed() : _value(0)
-{
-	std::cout << "Default constructor called" << '\n';
-}
+Fixed::Fixed() : _value(0) {}
 
-Fixed::Fixed(const Fixed& other) : _value(other.getRawBits())
-{
-	std::cout << "Copy constructor called" << '\n';
-}
+Fixed::Fixed(const Fixed& other) : _value(other.getRawBits()) {}
 
-Fixed::Fixed(const int num) : _value(num << Fixed::_fractional_bits)
-{
-	std::cout << "Int constructor called" << '\n';
-}
+Fixed::Fixed(const int num) : _value(num << Fixed::_fractional_bits) {}
 
 Fixed::Fixed(const float num) :
 	_value((int)roundf(num * (1 << Fixed::_fractional_bits)))
 {
-	std::cout << "Float constructor called" << '\n';
 }
 
-Fixed::~Fixed()
-{
-	std::cout << "Destructor called" << '\n';
-}
+Fixed::~Fixed() {}
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
-	std::cout << "Copy assignment operator called" << '\n';
 	if (this != &other) {
 		this->_value = other.getRawBits();
 	}
