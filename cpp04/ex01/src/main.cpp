4ee2f556897c8281918ffea7fb855793e16f5df6
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-11-21 00:46:42 +0100

    [04/01] test: Prove deep copy & improve printouts

--- cpp04/ex01/src/main.cpp
+++ cpp04/ex01/src/main.cpp
@@ -1,6 +1,7 @@
 // NOLINTBEGIN
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
 #include <iostream>
@@ -10,30 +11,42 @@ void delete_array(Animal* animals[]);
 
 int main()
 {
-	std::cout << "SUBJECT TEST CASES:" << '\n';
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	{
+		std::cout << "SUBJECT TEST CASE:\n" << '\n';
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
 
-	delete j; // should not create a leak
-	delete i;
-
-
-	std::cout << "\nMY TEST CASES:" << '\n';
-	Animal* animals[10];
-	fill_array(animals);
-	for (int i = 0; i < 10; ++i) {
-		animals[i]->makeSound();
+		delete j; // should not create a leak
+		delete i;
 	}
-	delete_array(animals);
+	std::cout << "\n\n--------------------------------------------\n\n" << '\n';
+	{
+		std::cout << "ARRAY OF ANIMAL POINTERS:\n" << '\n';
+		Animal* animals[10];
+		fill_array(animals);
+		for (int i = 0; i < 10; ++i) {
+			std::cout << animals[i]->getType() << ": ";
+			animals[i]->makeSound();
+		}
+		delete_array(animals);
+	}
+	std::cout << "\n\n--------------------------------------------\n\n" << '\n';
+	{
+		std::cout << "DEEP COPY OF BRAIN:\n" << '\n';
+		Cat cat1;
+		Cat cat2;
 
-	Animal* animal = new Animal();
-	Animal* dog = new Dog();
-	Animal* cat = new Cat();
-
-	delete animal;
-	delete dog;
-	delete cat;
-
+		for (int _ = 0; _ < Brain::brain_size; ++_) {
+			cat1.brain()->add_idea("An idea!");
+		}
+		std::cout << "\n🧠 CAT1 BRAIN 🧠" << '\n';
+		std::cout << *cat1.brain();
+		std::cout << "\n🧠 CAT2 BRAIN 🧠" << '\n';
+		std::cout << *cat2.brain() << '\n';
+		cat2 = cat1;
+		std::cout << "\n🧠 CAT2 BRAIN 🧠" << '\n';
+		std::cout << *cat2.brain();
+	}
 }
 
 void fill_array(Animal* animals[])
