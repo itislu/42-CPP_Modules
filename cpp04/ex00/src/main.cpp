2c999f36fc805263648342a69ca132e54eb4e4e5
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-11-21 01:04:07 +0100

    [04/00] test: Improve printouts

--- cpp04/ex00/src/main.cpp
+++ cpp04/ex00/src/main.cpp
@@ -9,49 +9,53 @@
 
 int main()
 {
-	std::cout << "SUBJECT TEST CASES:" << '\n';
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); // will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
+	{
+		std::cout << "SUBJECT TEST CASE:\n" << '\n';
+		const Animal* meta = new Animal();
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound(); // will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
 
-	delete meta;
-	delete i;
-	delete j;
+		delete meta;
+		delete i;
+		delete j;
+	}
+	std::cout << "\n\n--------------------------------------------\n\n" << '\n';
+	{
+		std::cout << "ANIMAL, DOG & CAT:\n" << '\n';
 
+		Animal* animal = new Animal();
+		Animal* dog = new Dog();
+		Animal* cat = new Cat();
+		std::cout << animal->getType() << ": ";
+		animal->makeSound();
+		std::cout << dog->getType() << ": ";
+		dog->makeSound();
+		std::cout << cat->getType() << ": ";
+		cat->makeSound();
 
-	std::cout << "\nMY TEST CASES:" << '\n';
+		delete animal;
+		delete dog;
+		delete cat;
+	}
+	std::cout << "\n\n--------------------------------------------\n\n" << '\n';
+	{
+		std::cout << "WRONG_ANIMAL & WRONG_CAT:\n" << '\n';
 
-	Animal* animal = new Animal();
-	Animal* dog = new Dog();
-	Animal* cat = new Cat();
-	std::cout << animal->getType() << ": ";
-	animal->makeSound();
-	std::cout << dog->getType() << ": ";
-	dog->makeSound();
-	std::cout << cat->getType() << ": ";
-	cat->makeSound();
+		WrongAnimal* wrong_animal = new WrongAnimal();
+		WrongAnimal* wrong_cat = new WrongCat();
+		std::cout << wrong_animal->getType() << ": ";
+		wrong_animal->makeSound();
+		std::cout << wrong_cat->getType() << ": ";
+		wrong_cat->makeSound();
 
-	delete animal;
-	delete dog;
-	delete cat;
-
-
-	std::cout << "\nMY WRONG_ANIMAL TEST CASES:" << '\n';
-
-	WrongAnimal* wrong_animal = new WrongAnimal();
-	WrongAnimal* wrong_cat = new WrongCat();
-	std::cout << wrong_animal->getType() << ": ";
-	wrong_animal->makeSound();
-	std::cout << wrong_cat->getType() << ": ";
-	wrong_cat->makeSound();
-
-	delete wrong_animal;
-	delete wrong_cat;
+		delete wrong_animal;
+		delete wrong_cat;
+	}
 }
 
 // NOLINTEND
