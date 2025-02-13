4290fa21ea0c7bf4259a6db13bb05490d80ee332
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-02-13 20:21:24 +0100

    [05/00] feat: Add log utils functions with colored labels

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -1,80 +1,94 @@
 #include "Bureaucrat.hpp"
 #include "GradeException.hpp"
+#include "grade.hpp"
+#include "utils.hpp"
 #include <exception>
 #include <iostream>
+#include <string>
 
 int main()
 {
+	int test = 1;
+
+	std::cerr << "\n---------------- " << test++ << " ----------------" << '\n';
+
 	try {
-		Bureaucrat bureaucrat("George", Bureaucrat::highest_grade - 1);
-		std::cout << bureaucrat << " constructed" << '\n';
+		Bureaucrat bureaucrat("George",
+		                      grade::increment(Bureaucrat::highest_grade));
+		std::cout << utils::log::ok(bureaucrat) << " constructed" << '\n';
 		bureaucrat.demote();
-		std::cout << bureaucrat << '\n';
+		std::cout << utils::log::ok(bureaucrat) << '\n';
 	}
 	catch (const std::exception& e) {
-		std::cout << e.what() << '\n';
+		std::cerr << utils::log::error(e.what()) << '\n';
 	}
 
-	std::cout << '\n';
+	std::cerr << "\n---------------- " << test++ << " ----------------" << '\n';
 
 	try {
-		Bureaucrat bureaucrat("George", Bureaucrat::lowest_grade + 1);
-		std::cout << bureaucrat << " constructed" << '\n';
+		Bureaucrat bureaucrat("George",
+		                      grade::decrement(Bureaucrat::lowest_grade));
+		std::cout << utils::log::ok(bureaucrat) << " constructed" << '\n';
 		bureaucrat.promote();
-		std::cout << bureaucrat << '\n';
+		std::cout << utils::log::ok(bureaucrat) << '\n';
 	}
 	catch (const std::exception& e) {
-		std::cout << e.what() << '\n';
+		std::cerr << utils::log::error(e.what()) << '\n';
 	}
 
-	std::cout << '\n';
+	std::cerr << "\n---------------- " << test++ << " ----------------" << '\n';
 
 	try {
 		Bureaucrat bureaucrat("George", Bureaucrat::highest_grade);
-		std::cout << bureaucrat << " constructed" << '\n';
+		std::cout << utils::log::ok(bureaucrat) << " constructed" << '\n';
 		bureaucrat.promote();
-		std::cout << bureaucrat << '\n';
+		std::cout << utils::log::ok(bureaucrat) << '\n';
 	}
 	catch (const std::exception& e) {
-		std::cout << e.what() << '\n';
+		std::cerr << utils::log::error(e.what()) << '\n';
 	}
 
-	std::cout << '\n';
+	std::cerr << "\n---------------- " << test++ << " ----------------" << '\n';
 
 	try {
 		Bureaucrat bureaucrat("George", Bureaucrat::lowest_grade);
-		std::cout << bureaucrat << " constructed" << '\n';
+		std::cout << utils::log::ok(bureaucrat) << " constructed" << '\n';
 		bureaucrat.demote();
-		std::cout << bureaucrat << '\n';
+		std::cout << utils::log::ok(bureaucrat) << '\n';
 	}
 	catch (const std::exception& e) {
-		std::cout << e.what() << '\n';
+		std::cerr << utils::log::error(e.what()) << '\n';
 	}
 
-	std::cout << '\n';
+	std::cerr << "\n---------------- " << test++ << " ----------------" << '\n';
 
 	try {
-		Bureaucrat bureaucrat("George", Bureaucrat::lowest_grade + 1);
-		std::cout << bureaucrat << " constructed" << '\n';
+		Bureaucrat bureaucrat("George",
+		                      grade::decrement(Bureaucrat::lowest_grade));
+		std::cout << utils::log::ok(bureaucrat) << " constructed" << '\n';
 		bureaucrat.promote();
-		std::cout << bureaucrat << '\n';
+		std::cout << utils::log::ok(bureaucrat) << '\n';
 	}
 	catch (const GradeException& e) {
-		std::cout << e.what() << '\n';
+		std::cerr << utils::log::error(e.what()) << '\n';
 	}
 
-	std::cout << '\n';
+	std::cerr << "\n---------------- " << test++ << " ----------------" << '\n';
 
 	try {
-		Bureaucrat bureaucrat("George", Bureaucrat::lowest_grade + 1);
-		std::cout << bureaucrat << " constructed" << '\n';
+		Bureaucrat bureaucrat("George",
+		                      grade::decrement(Bureaucrat::lowest_grade));
+		std::cout << utils::log::ok(bureaucrat) << " constructed" << '\n';
 		bureaucrat.promote();
-		std::cout << bureaucrat << '\n';
+		std::cout << utils::log::ok(bureaucrat) << '\n';
 	}
 	catch (const Bureaucrat::GradeTooHighException& e) {
-		std::cout << "SHOULD NOT GET HERE " << e.what() << '\n';
+		std::cerr << utils::log::error(std::string("SHOULD NOT GET HERE ")
+		                               + e.what());
 	}
 	catch (const Bureaucrat::GradeTooLowException& e) {
-		std::cout << e.what() << '\n';
+		std::cerr << utils::log::error(e.what()) << '\n';
 	}
+
+	std::cerr << '\n';
 }
