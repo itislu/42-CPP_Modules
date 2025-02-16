95754d6608d18197557c02244863ec2d2faa904c
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-02-16 21:30:10 +0100

    [05/02] feat: Add randomness test

--- cpp05/ex02/src/main.cpp
+++ cpp05/ex02/src/main.cpp
@@ -6,83 +6,113 @@
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "utils/log.hpp"
+#include "utils/string.hpp"
+#include <cstddef>
+#include <exception>
 #include <iostream>
+#include <streambuf>
+#include <string>
 
 int main()
 {
-	// {
-	// 	const Bureaucrat asterix("Asterix", Grade::highest_grade);
-	// 	RobotomyRequestForm r("Cleopatra");
-	// 	asterix.signForm(r);
+	int test = 0;
 
-	// 	for (int i = 0; i < 100000; ++i) {
-	// 		asterix.executeForm(r);
-	// 		std::cerr << "Robotomy score: " << r.score() << '\n';
-	// 	}
-	// 	return 0;
-	// }
+	std::cout << "\n---------------- " << ++test << " ----------------" << '\n';
+	try {
+		const Bureaucrat asterix("Asterix", Grade::highest_grade);
+		const Bureaucrat obelix("Obelix", Grade::lowest_grade);
+		ShrubberyCreationForm s("Caesar");
+		RobotomyRequestForm r("Cleopatra");
+		PresidentialPardonForm p("Brutus");
 
-	const Bureaucrat asterix("Asterix", Grade::highest_grade);
-	const Bureaucrat obelix("Obelix", Grade::lowest_grade);
-	ShrubberyCreationForm s("Caesar");
-	RobotomyRequestForm r("Cleopatra");
-	PresidentialPardonForm p("Brutus");
+		std::cout << '\n';
+		std::cout << s << '\n';
+		std::cout << r << '\n';
+		std::cout << p << '\n';
+		std::cout << '\n';
+		obelix.signForm(s);
+		obelix.signForm(r);
+		obelix.signForm(p);
+		std::cout << '\n';
+		std::cout << s << '\n';
+		std::cout << r << '\n';
+		std::cout << p << '\n';
+		std::cout << '\n';
+		obelix.executeForm(s);
+		obelix.executeForm(r);
+		obelix.executeForm(p);
+		asterix.executeForm(s);
+		asterix.executeForm(r);
+		asterix.executeForm(p);
+		std::cout << '\n';
+		std::cout << s << '\n';
+		std::cout << r << '\n';
+		std::cout << p << '\n';
+		std::cout << '\n';
+		asterix.signForm(s);
+		asterix.signForm(r);
+		asterix.signForm(p);
+		std::cout << '\n';
+		std::cout << s << '\n';
+		std::cout << r << '\n';
+		std::cout << p << '\n';
+		std::cout << '\n';
+		obelix.executeForm(s);
+		obelix.executeForm(r);
+		obelix.executeForm(p);
+		asterix.executeForm(s);
+		asterix.executeForm(r);
+		asterix.executeForm(p);
+	}
+	catch (const std::exception& e) {
+		std::cerr << utils::log::warn(std::string("SHOULD NOT GET HERE - ")
+		                              + e.what())
+		          << '\n';
+	}
 
-	std::cout << s << '\n';
-	std::cout << r << '\n';
-	std::cout << p << '\n';
-	std::cout << '\n';
-	obelix.signForm(s);
-	obelix.signForm(r);
-	obelix.signForm(p);
-	std::cout << '\n';
-	std::cout << s << '\n';
-	std::cout << r << '\n';
-	std::cout << p << '\n';
-	std::cout << '\n';
-	obelix.executeForm(s);
-	obelix.executeForm(r);
-	obelix.executeForm(p);
-	asterix.executeForm(s);
-	asterix.executeForm(r);
-	asterix.executeForm(p);
-	std::cout << '\n';
-	std::cout << s << '\n';
-	std::cout << r << '\n';
-	std::cout << p << '\n';
-	std::cout << '\n';
-	asterix.signForm(s);
-	asterix.signForm(r);
-	asterix.signForm(p);
-	std::cout << '\n';
-	std::cout << s << '\n';
-	std::cout << r << '\n';
-	std::cout << p << '\n';
-	std::cout << '\n';
-	obelix.executeForm(s);
-	obelix.executeForm(r);
-	obelix.executeForm(p);
-	// asterix.executeForm(s);
-	// asterix.executeForm(r);
-	// asterix.executeForm(p);
+	std::cout << "\n---------------- " << ++test << " ----------------" << '\n';
+	try {
+		const int repeats = 100000;
+		const Bureaucrat asterix("Asterix", Grade::highest_grade);
+		RobotomyRequestForm r("Cleopatra");
+		asterix.signForm(r);
 
-	// std::cout << "\n---------------- " << ++test << " ----------------" <<
-	// '\n'; try { 	const unsigned int grade_to_sign = 42; 	const unsigned int
-	// grade_to_exec = 43;
+		std::cerr << utils::log::info(std::string("Running robotomy ")
+		                              + utils::string::to_string(repeats)
+		                              + " times...")
+		          << '\n';
 
-	// 	const Bureaucrat asterix("Asterix", 42);
-	// 	const Bureaucrat obelix("Obelix", 43);
-	// 	Form form("Passierschein A38", grade_to_sign, grade_to_exec);
+		std::streambuf* og_cout = std::cout.rdbuf();
+		std::streambuf* og_cerr = std::cerr.rdbuf();
+		std::cout.rdbuf(NULL);
+		std::cerr.rdbuf(NULL);
 
-	// 	std::cout << '\n' << form << "\n\n";
-	// 	obelix.signForm(form);
-	// 	std::cout << '\n' << form << "\n\n";
-	// 	asterix.signForm(form);
-	// 	std::cout << '\n' << form << '\n';
-	// }
-	// catch (const std::exception& e) {
-	// 	std::cerr << utils::log::error(e.what()) << '\n';
-	// }
+		try {
+			for (int i = 0; i < repeats; ++i) {
+				asterix.executeForm(r);
+			}
+		}
+		catch (...) {
+			std::cout.rdbuf(og_cout);
+			std::cerr.rdbuf(og_cerr);
+			throw;
+		}
+
+		std::cout.rdbuf(og_cout);
+		std::cerr.rdbuf(og_cerr);
+
+		std::cerr << utils::log::info("Robotomy score: "
+		                              + utils::string::to_string(r.score()))
+		          << '\n';
+	}
+	catch (const std::exception& e) {
+		std::cerr << utils::log::warn(std::string("SHOULD NOT GET HERE - ")
+		                              + e.what())
+		          << '\n';
+	}
+
+	std::cout << '\n';
 }
 
 // NOLINTEND(readability-magic-numbers)
