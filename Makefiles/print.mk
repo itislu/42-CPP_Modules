itislu/42-CPP_Modules

commit 8e4e2b724d017bb3416262190d023218a91900c8

build: Modularize documentation and help targets and printouts
Also add include guards to fix multiple appends to mode message.

Makefiles/print.mk
@@ -1,0 +1,145 @@
+ifndef PRINT_MK
+PRINT_MK		:=	1
+
+include				$(shell git rev-parse --show-toplevel)/Makefiles/setup.mk
+
+
+# ********************************* COLORS *********************************** #
+
+STY_RES			:=	"\e[0m"
+STY_BOL			:=	"\e[1m"
+STY_ITA			:=	"\e[3m"
+STY_UND			:=	"\e[4m"
+STY_RED			:=	"\e[31m"
+STY_GRE			:=	"\e[32m"
+STY_YEL			:=	"\e[33m"
+STY_BLU			:=	"\e[34m"
+STY_MAG			:=	"\e[35m"
+STY_CYA			:=	"\e[36m"
+STY_WHI			:=	"\e[37m"
+STY_GRA			:=	"\e[90m"
+STY_WHI_BRI		:=	"\e[97m"
+STY_BLA_BG		:=	"\e[40m"
+STY_RED_BG		:=	"\e[41m"
+STY_GRE_BG		:=	"\e[42m"
+STY_YEL_BG		:=	"\e[43m"
+STY_BLU_BG		:=	"\e[44m"
+STY_MAG_BG		:=	"\e[45m"
+STY_CYA_BG		:=	"\e[46m"
+STY_WHI_BG		:=	"\e[47m"
+STY_GRA_BG		:=	"\e[100m"
+STY_WHI_BRI_BG	:=	"\e[107m"
+
+
+# **************************** PRINT FUNCTIONS ******************************* #
+
+define PRINT
+if [[ -n "$(shell printf '%b' "$(1)")" ]]; then \
+	echo -e -n "$(1)" \
+		| awk -v prefix="$(shell printf '%b' $(MSG_PREFIX))" '{print (prefix ? prefix " " : "") $$0}' \
+		| tr -d '\n'; \
+fi
+endef
+
+define PRINTLN
+if [[ -n "$(shell printf '%b' "$(1)")" ]]; then \
+	echo -e "$(1)" \
+		| awk -v prefix="$(shell printf '%b' $(MSG_PREFIX))" '{print (prefix ? prefix " " : "") $$0}'; \
+fi
+endef
+
+
+# **************************** CUSTOM MESSAGES ******************************* #
+
+#	Adjustments to dynamic messages
+
+ifneq (, $(MSG_PREFIX))
+MSG_PREFIX		:=	$(STY_WHI)$(MSG_PREFIX)$(STY_RES)
+endif
+
+ifneq (, $(MSG_USAGE))
+MSG_USAGE		:=	"\n"$(MSG_USAGE)
+endif
+
+
+########################## Top-level only messages #############################
+ifeq (0, $(MAKELEVEL))
+
+#	Make status messages
+
+MSG_MAKE_INFO	:=	$(STY_ITA)$(STY_WHI)" Make version: $(MAKE_VERSION)"$(STY_RES)
+
+MSG_COMP_INFO	:=	$(STY_ITA)$(STY_WHI)" Compiler version: $(CXX_VERSION)"$(STY_RES)
+
+MSG_HELP		:=	$(STY_ITA)$(STY_WHI)" Run 'make help' to see all available Makefile targets."$(STY_RES)
+
+ifneq (, $(filter $(REBUILD_TARGETS),$(MAKECMDGOALS) $(MODE)))
+MSG_START		:=	$(STY_ITA)"Rebuilding $(NAME) ... "$(STY_RES)
+else
+MSG_START		:=	$(STY_ITA)"Building $(NAME) ... "$(STY_RES)
+endif
+
+MSG_SUCCESS		?=	$(STY_BOL)$(STY_ITA)$($(COLOR_MAKE))"DONE!"$(STY_RES)
+
+MSG_NO_CHANGE	:=	$(STY_ITA)$(STY_WHI)"Everything up-to-date!"$(STY_RES)
+
+MSG_FAILURE		:=	$(STY_BOL)$(STY_ITA)$(STY_RED)"BUILD FAILED!"$(STY_RES)
+
+
+#	Build modes
+
+MSG_RUN			:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_YEL)"~~~~~~~~~~~~~~~~~~~~~~~ RUN MODE ~~~~~~~~~~~~~~~~~~~~~~~"$(STY_RES)
+ifneq (, $(filter run,$(MAKECMDGOALS) $(MODE)))
+MSG_MODE		:=	$(MSG_MODE)$(MSG_RUN)
+endif
+
+MSG_OPT			:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_GRE)"~~~~~~~~~~~~~~~~~~~ OPTIMIZATION MODE ~~~~~~~~~~~~~~~~~~"$(STY_RES)
+ifneq (, $(filter opt,$(MAKECMDGOALS) $(MODE)))
+MSG_MODE		:=	$(MSG_MODE)$(MSG_OPT)
+endif
+
+MSG_SAN			:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_RED)"~~~~~~~~~~~~~~~~~~~~ SANITIZER MODE ~~~~~~~~~~~~~~~~~~~~"$(STY_RES)
+ifneq (, $(filter san,$(MAKECMDGOALS) $(MODE)))
+MSG_MODE		:=	$(MSG_MODE)$(MSG_SAN)
+endif
+
+MSG_VAL			:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_BLU)"~~~~~~~~~~~~~~~~~~~~~ VALGRIND MODE ~~~~~~~~~~~~~~~~~~~~"$(STY_RES)
+ifneq (, $(filter val,$(MAKECMDGOALS) $(MODE)))
+MSG_MODE		:=	$(MSG_MODE)$(MSG_VAL)
+endif
+
+MSG_VALFD		:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_CYA)"~~~~~~~~~~~~~~~~~~~ VALGRIND FD MODE ~~~~~~~~~~~~~~~~~~~"$(STY_RES)
+ifneq (, $(filter valfd,$(MAKECMDGOALS) $(MODE)))
+MSG_MODE		:=	$(MSG_MODE)$(MSG_VALFD)
+endif
+
+MSG_TERM		:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_YEL)"~~~~~~~~~~~~~~~~~~~ NEW TERMINAL MODE ~~~~~~~~~~~~~~~~~~"$(STY_RES)
+ifneq (, $(filter term,$(MAKECMDGOALS) $(MODE)))
+MSG_MODE		:=	$(MSG_MODE)$(MSG_TERM)
+endif
+
+MSG_CLEAR		:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_GRA)"~~~~~~~~~~~~~~~~~~~~~ CLEAR MODE ~~~~~~~~~~~~~~~~~~~~"$(STY_RES)
+ifneq (, $(filter clear,$(MAKECMDGOALS) $(MODE)))
+MSG_MODE		:=	$(MSG_MODE)$(MSG_CLEAR)
+endif
+
+
+#	Clean messages
+
+MSG_CLEAN		:=	$(STY_ITA)"Cleaning up build artifacts ... "$(STY_RES)
+
+MSG_FCLEAN		:=	$(STY_ITA)"Cleaning up build artifacts and executable ... "$(STY_RES)
+
+MSG_FFCLEAN		:=	$(STY_ITA)"Forcefully cleaning up build artifacts directory, executable and generated documentation ... "$(STY_RES)
+
+endif
+################################################################################
+
+
+# *************************** MAKEFILE DEBUGGING ***************************** #
+
+print-%			:
+					$(call PRINTLN,$* = $(shell echo -e -n $($*)))
+
+
+endif

Makefiles/build.mk
@@ -1,4 +1,8 @@
+ifndef BUILD_MK
+BUILD_MK		:=	1
+
 include				$(shell git rev-parse --show-toplevel)/Makefiles/setup.mk
+include				$(MAKEFILES_DIR)/print.mk
 
 
 # *************************** BUILD PREPARATION ****************************** #
@@ -166,106 +170,6 @@ RUN				:=	true
 endif
 
 
-# ************************* DOCUMENTATION TARGETS **************************** #
-
-BEAR_IMG		:=	bear
-DOXYGEN_IMG		:=	doxygen
-CLANG_UML_IMG	:=	clang-uml
-PLANTUML_IMG	:=	plantuml
-DOXYFILE		:=	$(REPO_ROOT)/Doxyfile
-CLANG_UML_CFG	:=	$(REPO_ROOT)/.clang-uml
-DOXYGEN_OUTDIR	:=	$(DOC_DIR)/doxygen
-UML_OUTDIR		:=	$(DOC_DIR)/uml
-
-
-bear			:	.bear-image
-					docker run --rm \
-						-v $(REPO_ROOT):$(REPO_ROOT) \
-						-w $(PWD) \
-						$(BEAR_IMG) \
-						bash -c '{ bear -- make re; make fclean; } >/dev/null'
-					echo
-					$(call PRINTLN,"Generated compile_commands.json.")
-
-.bear-image		:
-					docker build -t $(BEAR_IMG) $(DOCKER_DIR)/$(BEAR_IMG)
-
-doxygen			:	.doxygen-image bear $(DOXYFILE)
-					mkdir -p $(DOXYGEN_OUTDIR)
-					docker run --rm \
-						-v $(REPO_ROOT):$(REPO_ROOT) \
-						-w $(PWD) \
-						$(DOXYGEN_IMG) \
-						bash -c '{ cat $(DOXYFILE); \
-							echo PROJECT_NAME="[$(REPO_SUBDIR)] - $(NAME)"; \
-							echo OUTPUT_DIRECTORY=$(DOXYGEN_OUTDIR); \
-							} | doxygen -q -'
-					echo
-					$(call PRINTLN,"Generated Doxygen documentation in $(DOXYGEN_OUTDIR).")
-					open $(DOXYGEN_OUTDIR)/html/index.html
-
-$(DOXYFILE)		:	| .doxygen-image
-					docker run --rm \
-						-v $(REPO_ROOT):$(REPO_ROOT) \
-						-w $(PWD) \
-						$(DOXYGEN_IMG) \
-						doxygen -g $(DOXYFILE)
-					echo
-					$(call PRINTLN,"Created default Doxyfile. Please review and adjust settings as needed$(COMMA) then rerun.")
-					exit 1
-
-.doxygen-image	:
-					docker build -t $(DOXYGEN_IMG) $(DOCKER_DIR)/$(DOXYGEN_IMG)
-
-uml				:	.clang-uml .plantuml-image
-					$(MAKE) .plantuml
-
-.clang-uml		:	.clang-uml-image bear $(CLANG_UML_CFG)
-					mkdir -p $(UML_OUTDIR)
-					docker run --rm \
-						-v $(REPO_ROOT):$(REPO_ROOT) \
-						-w $(PWD) \
-						$(CLANG_UML_IMG) \
-						clang-uml --progress --paths-relative-to-pwd --config=$(CLANG_UML_CFG) --output-directory=$(UML_OUTDIR)
-					echo
-					$(call PRINTLN,"Generated PlantUML files in $(UML_OUTDIR).")
-
-$(CLANG_UML_CFG):	| .clang-uml-image
-					docker run --rm \
-						-v $(REPO_ROOT):$(REPO_ROOT) \
-						-w $(PWD) \
-						$(CLANG_UML_IMG) \
-						clang-uml --init
-					mv .clang-uml $(CLANG_UML_CFG)
-					echo
-					$(call PRINTLN,"Created default .clang-uml configuration file. Please review and adjust settings as needed$(COMMA) then rerun.")
-					exit 1
-
-.clang-uml-image	:
-					docker build -t $(CLANG_UML_IMG) $(DOCKER_DIR)/$(CLANG_UML_IMG)
-
-.plantuml		:	.plantuml-image
-					$(call PRINTLN,"Converting PlantUML files to PNG and SVG ...")
-					mkdir -p $(UML_OUTDIR)
-					docker run --rm \
-						-v $(REPO_ROOT):$(REPO_ROOT) \
-						-w $(PWD) \
-						$(PLANTUML_IMG) \
-						plantuml -tpng -tsvg "$(UML_OUTDIR)/*.puml"
-					$(call PRINTLN,"Generated PNG and SVG files in $(UML_OUTDIR).")
-					open $(UML_OUTDIR)
-					$(call PRINTLN,"Converting PlantUML files to PDF '('this may take a moment')' ...")
-					docker run --rm \
-						-v $(REPO_ROOT):$(REPO_ROOT) \
-						-w $(PWD) \
-						$(PLANTUML_IMG) \
-						plantuml -tpdf "$(UML_OUTDIR)/*.puml"
-					$(call PRINTLN,"Generated PDF files in $(UML_OUTDIR).")
-
-.plantuml-image	:
-					docker build -t $(PLANTUML_IMG) $(DOCKER_DIR)/$(PLANTUML_IMG)
-
-
 # ***************************** CLEAN TARGETS ******************************** #
 
 clean			:
@@ -292,290 +196,4 @@ ffclean			:
 					$(call PRINTLN,"$(MSG_SUCCESS)")
 
 
-# ****************************** HELP TARGETS ******************************** #
-
-help			:
-					echo -e "Targets:"
-					echo -e "  all              Build the project (default target)"
-					echo -e "  run              Build and run the project"
-					echo -e "  opt              Rebuild the project with optimizations"
-					echo -e "  san              Rebuild the project with sanitizers"
-					echo -e "  val              Build and run the project with valgrind"
-					echo -e "  valfd            Build and run the project with valgrind and file descriptor tracking"
-					echo -e "  term             Build and run the project in a new terminal window"
-					echo -e "  clear            Build the project and clear the terminal"
-					echo -e "  re               Rebuild the project"
-					echo -e "  bear             Generate compilation database using Bear"
-					echo -e "  doxygen          Generate documentation using Doxygen"
-					echo -e "  uml              Generate UML diagrams using clang-uml and convert them to PNG, SVG and PDF"
-					echo -e "  clean            Remove build artifacts"
-					echo -e "  fclean           Remove build artifacts and executable"
-					echo -e "  ffclean          Remove build artifacts, executable and generated documentation without checking for unknown files"
-					echo -e "  print-%          Print the value of a Makefile variable (replace % with variable name)"
-					echo -e "  help             Display this message"
-					echo -e "  help-% | %-help  Display more information for a specific target (replace % with target name)"
-					echo
-					echo -e "Environment Variables:"
-					echo -e "  MODE             Build mode to combine multiple targets"
-					echo -e "  ARGS             If specified, the program will run with those arguments after compilation."
-					echo -e "  TERMINAL         Terminal emulator to use for targets opening a new terminal window"
-					echo
-					echo -e "Usage: make [\\$(STY_UND)target\\$(STY_RES)] [MODE=\"<\\$(STY_UND)mode1\\$(STY_RES)> [\\$(STY_UND)mode2\\$(STY_RES)] [...]\"] [ARGS=\"'<\\$(STY_UND)arg1\\$(STY_RES)>' '[\\$(STY_UND)arg2\\$(STY_RES)]' '[...]'\"] [TERMINAL=<\\$(STY_UND)terminal\\$(STY_RES)>]"
-
-help-all		:
-					echo -e "Build the project."
-					echo -e "This is the default target when no target is specified."
-
-help-run		:
-					echo -e "Build the project and run the executable."
-					echo -e "Arguments to the program can be passed via the ARGS variable."
-					echo
-					echo -e "Usage: make run [ARGS=\"'<\\$(STY_UND)arg1\\$(STY_RES)>' '[\\$(STY_UND)arg2\\$(STY_RES)]' '[...]'\"]"
-
-help-opt		:
-					echo -e "Rebuild the project with the following compiler optimization flags:"
-					echo -e "  $(CXXFLAGS_OPT)"
-
-help-san		:
-					echo -e "Rebuild the project with the following sanitizer flags:"
-					echo -e "  $(CXXFLAGS_SAN)"
-
-help-val		:
-					echo -e "Build the project and run the executable with valgrind."
-					echo -e "Arguments to the program can be passed via the ARGS variable."
-					echo
-					echo -e "The following valgrind flags are used:"
-					echo -e "$(VALGRINDFLAGS)" | tr ' ' '\n' | sed 's/^/  /'
-					echo
-					echo -e "Usage: make val [ARGS=\"'<\\$(STY_UND)arg1\\$(STY_RES)>' '[\\$(STY_UND)arg2\\$(STY_RES)]' '[...]'\"]"
-
-help-valfd		:
-					echo -e "Build the project and run the executable with valgrind and file descriptor tracking."
-					echo -e "A new terminal window is opened to avoid inheriting open file descriptors."
-					echo -e "Arguments to the program can be passed via the ARGS variable."
-					echo
-					echo -e "The following valgrind flags are used:"
-					echo -e "$(VALGRINDFLAGS)" | tr ' ' '\n' | sed 's/^/  /'
-					echo -e "File descriptor specific flags:"
-					echo -e "$(VALGRINDFDFLAGS)" | tr ' ' '\n' | sed 's/^/  /'
-					echo
-					echo -e "Usage: make valfd [ARGS=\"'<\\$(STY_UND)arg1\\$(STY_RES)>' '[\\$(STY_UND)arg2\\$(STY_RES)]' '[...]'\"]"
-
-help-term		:
-					echo -e "Build the project and run the executable in a new terminal window."
-					echo -e "The terminal emulator used is determined by the TERMINAL variable."
-					echo -e "Arguments to the program can be passed via the ARGS variable."
-					echo
-					echo -e "The following terminal emulator is used by default:"
-					echo -e "  $(TERMINAL)"
-					echo
-					echo -e "Usage: make term [TERMINAL=<\\$(STY_UND)terminal\\$(STY_RES)>] [ARGS=\"'<\\$(STY_UND)arg1\\$(STY_RES)>' '[\\$(STY_UND)arg2\\$(STY_RES)]' '[...]'\"]"
-
-help-clear		:
-					echo -e "Build the project and clear the terminal."
-
-help-re			:
-					echo -e "Rebuild the project."
-
-help-bear		:
-					echo -e "Generate compilation database (compile_commands.json) using Bear."
-					echo -e "The compilation database is used by tools like clangd to significantly speed up workspace parsing."
-					echo -e "It is also required for tools like doxygen or clang-uml that need compilation information to analyze the codebase."
-					echo -e "The compilation database is generated by running 'bear -- make re'."
-
-help-doxygen	:
-					echo -e "Generate documentation using Doxygen."
-					echo -e "A Doxyfile is required to configure the documentation generation."
-					echo -e "If no Doxyfile exists, a default configuration file will be created."
-					echo -e "The documentation will be generated in $(DOXYGEN_OUTDIR)."
-					echo -e "After generation, the HTML documentation will be opened in the default browser."
-
-help-uml		:
-					echo -e "Generate UML diagrams using clang-uml and convert them to PNG, SVG and PDF."
-					echo -e "A .clang-uml configuration file is required to configure the UML generation."
-					echo -e "If no .clang-uml configuration file exists, a default configuration file will be created."
-					echo -e "The generated files will be saved in $(UML_OUTDIR)."
-					echo -e "After generation, the output directory will be opened."
-
-help-clean		:
-					echo -e "Remove build artifacts."
-
-help-fclean		:
-					echo -e "Remove build artifacts and the executable."
-
-help-ffclean	:
-					echo -e "Remove build artifacts, the executable and generated documentation without checking for unknown files."
-					echo
-					echo -e "The following directories will be removed:"
-					echo -e "  $(OBJ_DIR)"
-					echo -e "  $(DEP_DIR)"
-					echo -e "  $(DOXYGEN_OUTDIR)"
-					echo -e "  $(UML_OUTDIR)"
-
-help-print		:
-					echo -e "Print the value of a Makefile variable by appending the variable name to print-..."
-					echo -e "Useful for Makefile debugging."
-					echo
-					echo -e "Usage: make print-<\\$(STY_UND)variable name\\$(STY_RES)>"
-
-help-help		:
-					echo -e "Display more information for a specific target by appending or prepending help."
-					echo
-					echo -e "Usage: make help-<\\$(STY_UND)target\\$(STY_RES)> | make <\\$(STY_UND)target\\$(STY_RES)>-help"
-
-help-MODE MODE-help:
-					echo -e "Build mode to combine with other targets."
-					echo -e "Multiple modes can be combined by separating them with a space."
-					echo
-					echo -e "Usage: make <\\$(STY_UND)target\\$(STY_RES)> MODE=\"<\\$(STY_UND)mode1\\$(STY_RES)> [\\$(STY_UND)mode2\\$(STY_RES)] [...]\""
-
-help-ARGS ARGS-help:
-					echo -e "If specified, the program will run with those arguments after compilation."
-					echo
-					echo -e "Usage: make <\\$(STY_UND)target\\$(STY_RES)> ARGS=\"'<\\$(STY_UND)arg1\\$(STY_RES)>' '[\\$(STY_UND)arg2\\$(STY_RES)]' '[...]'\""
-
-help-TERMINAL TERMINAL-help:
-					echo -e "Override the default terminal emulator for targets opening a new terminal window."
-					echo
-					echo -e "Usage: make <\\$(STY_UND)target\\$(STY_RES)> TERMINAL=<\\$(STY_UND)terminal\\$(STY_RES)>"
-
-%-help:
-					$(MAKE) help-$(subst -help,,$@)
-
-
-# ********************************* COLORS *********************************** #
-
-STY_RES			:=	"\e[0m"
-STY_BOL			:=	"\e[1m"
-STY_ITA			:=	"\e[3m"
-STY_UND			:=	"\e[4m"
-STY_RED			:=	"\e[31m"
-STY_GRE			:=	"\e[32m"
-STY_YEL			:=	"\e[33m"
-STY_BLU			:=	"\e[34m"
-STY_MAG			:=	"\e[35m"
-STY_CYA			:=	"\e[36m"
-STY_WHI			:=	"\e[37m"
-STY_GRA			:=	"\e[90m"
-STY_WHI_BRI		:=	"\e[97m"
-STY_BLA_BG		:=	"\e[40m"
-STY_RED_BG		:=	"\e[41m"
-STY_GRE_BG		:=	"\e[42m"
-STY_YEL_BG		:=	"\e[43m"
-STY_BLU_BG		:=	"\e[44m"
-STY_MAG_BG		:=	"\e[45m"
-STY_CYA_BG		:=	"\e[46m"
-STY_WHI_BG		:=	"\e[47m"
-STY_GRA_BG		:=	"\e[100m"
-STY_WHI_BRI_BG	:=	"\e[107m"
-
-
-# **************************** CUSTOM MESSAGES ******************************* #
-
-#	Print functions
-
-define PRINT
-if [[ -n "$(shell printf '%b' "$(1)")" ]]; then \
-	echo -e -n "$(1)" \
-		| awk -v prefix="$(shell printf '%b' $(MSG_PREFIX))" '{print (prefix ? prefix " " : "") $$0}' \
-		| tr -d '\n'; \
-fi
-endef
-
-define PRINTLN
-if [[ -n "$(shell printf '%b' "$(1)")" ]]; then \
-	echo -e "$(1)" \
-		| awk -v prefix="$(shell printf '%b' $(MSG_PREFIX))" '{print (prefix ? prefix " " : "") $$0}'; \
-fi
-endef
-
-
-#	Adjustments to dynamic messages
-
-ifneq (, $(MSG_PREFIX))
-MSG_PREFIX		:=	$(STY_WHI)$(MSG_PREFIX)$(STY_RES)
 endif
-
-ifneq (, $(MSG_USAGE))
-MSG_USAGE		:=	"\n"$(MSG_USAGE)
-endif
-
-
-########################## Top-level only messages #############################
-ifeq (0, $(MAKELEVEL))
-
-#	Make status messages
-
-MSG_MAKE_INFO	:=	$(STY_ITA)$(STY_WHI)" Make version: $(MAKE_VERSION)"$(STY_RES)
-
-MSG_COMP_INFO	:=	$(STY_ITA)$(STY_WHI)" Compiler version: $(CXX_VERSION)"$(STY_RES)
-
-MSG_HELP		:=	$(STY_ITA)$(STY_WHI)" Run 'make help' to see all available Makefile targets."$(STY_RES)
-
-ifneq (, $(filter $(REBUILD_TARGETS),$(MAKECMDGOALS) $(MODE)))
-MSG_START		:=	$(STY_ITA)"Rebuilding $(NAME) ... "$(STY_RES)
-else
-MSG_START		:=	$(STY_ITA)"Building $(NAME) ... "$(STY_RES)
-endif
-
-MSG_SUCCESS		?=	$(STY_BOL)$(STY_ITA)$($(COLOR_MAKE))"DONE!"$(STY_RES)
-
-MSG_NO_CHANGE	:=	$(STY_ITA)$(STY_WHI)"Everything up-to-date!"$(STY_RES)
-
-MSG_FAILURE		:=	$(STY_BOL)$(STY_ITA)$(STY_RED)"BUILD FAILED!"$(STY_RES)
-
-
-#	Build modes
-
-MSG_RUN			:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_YEL)"~~~~~~~~~~~~~~~~~~~~~~~ RUN MODE ~~~~~~~~~~~~~~~~~~~~~~~"$(STY_RES)
-ifneq (, $(filter run,$(MAKECMDGOALS) $(MODE)))
-MSG_MODE		:=	$(MSG_MODE)$(MSG_RUN)
-endif
-
-MSG_OPT			:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_GRE)"~~~~~~~~~~~~~~~~~~~ OPTIMIZATION MODE ~~~~~~~~~~~~~~~~~~"$(STY_RES)
-ifneq (, $(filter opt,$(MAKECMDGOALS) $(MODE)))
-MSG_MODE		:=	$(MSG_MODE)$(MSG_OPT)
-endif
-
-MSG_SAN			:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_RED)"~~~~~~~~~~~~~~~~~~~~ SANITIZER MODE ~~~~~~~~~~~~~~~~~~~~"$(STY_RES)
-ifneq (, $(filter san,$(MAKECMDGOALS) $(MODE)))
-MSG_MODE		:=	$(MSG_MODE)$(MSG_SAN)
-endif
-
-MSG_VAL			:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_BLU)"~~~~~~~~~~~~~~~~~~~~~ VALGRIND MODE ~~~~~~~~~~~~~~~~~~~~"$(STY_RES)
-ifneq (, $(filter val,$(MAKECMDGOALS) $(MODE)))
-MSG_MODE		:=	$(MSG_MODE)$(MSG_VAL)
-endif
-
-MSG_VALFD		:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_CYA)"~~~~~~~~~~~~~~~~~~~ VALGRIND FD MODE ~~~~~~~~~~~~~~~~~~~"$(STY_RES)
-ifneq (, $(filter valfd,$(MAKECMDGOALS) $(MODE)))
-MSG_MODE		:=	$(MSG_MODE)$(MSG_VALFD)
-endif
-
-MSG_TERM		:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_YEL)"~~~~~~~~~~~~~~~~~~~ NEW TERMINAL MODE ~~~~~~~~~~~~~~~~~~"$(STY_RES)
-ifneq (, $(filter term,$(MAKECMDGOALS) $(MODE)))
-MSG_MODE		:=	$(MSG_MODE)$(MSG_TERM)
-endif
-
-MSG_CLEAR		:=	$(STY_BOL)$(STY_ITA)$(STY_UND)$(STY_GRA)"~~~~~~~~~~~~~~~~~~~~~ CLEAR MODE ~~~~~~~~~~~~~~~~~~~~"$(STY_RES)
-ifneq (, $(filter clear,$(MAKECMDGOALS) $(MODE)))
-MSG_MODE		:=	$(MSG_MODE)$(MSG_CLEAR)
-endif
-
-
-#	Clean messages
-
-MSG_CLEAN		:=	$(STY_ITA)"Cleaning up build artifacts ... "$(STY_RES)
-
-MSG_FCLEAN		:=	$(STY_ITA)"Cleaning up build artifacts and executable ... "$(STY_RES)
-
-MSG_FFCLEAN		:=	$(STY_ITA)"Forcefully cleaning up build artifacts directory, executable and generated documentation ... "$(STY_RES)
-
-endif
-################################################################################
-
-
-# *************************** MAKEFILE DEBUGGING ***************************** #
-
-print-%			:
-					$(call PRINTLN,$* = $(shell echo -e -n $($*)))

Makefiles/config.mk
@@ -1,3 +1,6 @@
+ifndef CONFIG_MK
+CONFIG_MK		:=	1
+
 include				$(shell git rev-parse --show-toplevel)/Makefiles/setup.mk
 
 
@@ -89,4 +92,7 @@ TERMINALFLAGS	?=	$(if $(filter gnome-terminal,$(TERMINAL)), --title="$(TERMINALT
 
 ifndef NAME
     $(error NAME is not defined)
+endif
+
+
 endif

Makefiles/doc.mk
@@ -1,0 +1,108 @@
+ifndef DOC_MK
+DOC_MK			:=	1
+
+include				$(shell git rev-parse --show-toplevel)/Makefiles/setup.mk
+include				$(MAKEFILES_DIR)/print.mk
+
+
+# ************************* DOCUMENTATION TARGETS **************************** #
+
+BEAR_IMG		:=	bear
+DOXYGEN_IMG		:=	doxygen
+CLANG_UML_IMG	:=	clang-uml
+PLANTUML_IMG	:=	plantuml
+DOXYFILE		:=	$(REPO_ROOT)/Doxyfile
+CLANG_UML_CFG	:=	$(REPO_ROOT)/.clang-uml
+DOXYGEN_OUTDIR	:=	$(DOC_DIR)/doxygen
+UML_OUTDIR		:=	$(DOC_DIR)/uml
+
+
+bear			:	.bear-image
+					docker run --rm \
+						-v $(REPO_ROOT):$(REPO_ROOT) \
+						-w $(PWD) \
+						$(BEAR_IMG) \
+						bash -c '{ bear -- make re; make fclean; } >/dev/null'
+					echo
+					$(call PRINTLN,"Generated compile_commands.json.")
+
+.bear-image		:
+					docker build -t $(BEAR_IMG) $(DOCKER_DIR)/$(BEAR_IMG)
+
+doxygen			:	.doxygen-image bear $(DOXYFILE)
+					mkdir -p $(DOXYGEN_OUTDIR)
+					docker run --rm \
+						-v $(REPO_ROOT):$(REPO_ROOT) \
+						-w $(PWD) \
+						$(DOXYGEN_IMG) \
+						bash -c '{ cat $(DOXYFILE); \
+							echo PROJECT_NAME="[$(REPO_SUBDIR)] - $(NAME)"; \
+							echo OUTPUT_DIRECTORY=$(DOXYGEN_OUTDIR); \
+							} | doxygen -q -'
+					echo
+					$(call PRINTLN,"Generated Doxygen documentation in $(DOXYGEN_OUTDIR).")
+					open $(DOXYGEN_OUTDIR)/html/index.html
+
+$(DOXYFILE)		:	| .doxygen-image
+					docker run --rm \
+						-v $(REPO_ROOT):$(REPO_ROOT) \
+						-w $(PWD) \
+						$(DOXYGEN_IMG) \
+						doxygen -g $(DOXYFILE)
+					echo
+					$(call PRINTLN,"Created default Doxyfile. Please review and adjust settings as needed$(COMMA) then rerun.")
+					exit 1
+
+.doxygen-image	:
+					docker build -t $(DOXYGEN_IMG) $(DOCKER_DIR)/$(DOXYGEN_IMG)
+
+uml				:	.clang-uml .plantuml-image
+					$(MAKE) .plantuml
+
+.clang-uml		:	.clang-uml-image bear $(CLANG_UML_CFG)
+					mkdir -p $(UML_OUTDIR)
+					docker run --rm \
+						-v $(REPO_ROOT):$(REPO_ROOT) \
+						-w $(PWD) \
+						$(CLANG_UML_IMG) \
+						clang-uml --progress --paths-relative-to-pwd --config=$(CLANG_UML_CFG) --output-directory=$(UML_OUTDIR)
+					echo
+					$(call PRINTLN,"Generated PlantUML files in $(UML_OUTDIR).")
+
+$(CLANG_UML_CFG):	| .clang-uml-image
+					docker run --rm \
+						-v $(REPO_ROOT):$(REPO_ROOT) \
+						-w $(PWD) \
+						$(CLANG_UML_IMG) \
+						clang-uml --init
+					mv .clang-uml $(CLANG_UML_CFG)
+					echo
+					$(call PRINTLN,"Created default .clang-uml configuration file. Please review and adjust settings as needed$(COMMA) then rerun.")
+					exit 1
+
+.clang-uml-image	:
+					docker build -t $(CLANG_UML_IMG) $(DOCKER_DIR)/$(CLANG_UML_IMG)
+
+.plantuml		:	.plantuml-image
+					$(call PRINTLN,"Converting PlantUML files to PNG and SVG ...")
+					mkdir -p $(UML_OUTDIR)
+					docker run --rm \
+						-v $(REPO_ROOT):$(REPO_ROOT) \
+						-w $(PWD) \
+						$(PLANTUML_IMG) \
+						plantuml -tpng -tsvg "$(UML_OUTDIR)/*.puml"
+					$(call PRINTLN,"Generated PNG and SVG files in $(UML_OUTDIR).")
+					open $(UML_OUTDIR)
+					$(call PRINTLN,"Converting PlantUML files to PDF '('this may take a moment')' ...")
+					docker run --rm \
+						-v $(REPO_ROOT):$(REPO_ROOT) \
+						-w $(PWD) \
+						$(PLANTUML_IMG) \
+						plantuml -tpdf "$(UML_OUTDIR)/*.puml"
+					$(call PRINTLN,"Generated PDF files in $(UML_OUTDIR).")
+
+.plantuml-image	:
+					docker build -t $(PLANTUML_IMG) $(DOCKER_DIR)/$(PLANTUML_IMG)
+
+
+endif

commit e66e18d7e2247b067fa82d5e29514c88a3472da7

build: Fix recompilation message not showing correctly
`make re` still printed "Building" instead of "Rebuilding".

Bug was introduced in 8e4e2b724d017bb3416262190d023218a91900c8.

Makefiles/print.mk
@@ -74,11 +74,7 @@ MSG_COMP_INFO	:=	$(STY_ITA)$(STY_WHI)"Compiler version: $(CXX_VERSION)"$(STY_RES
 
 MSG_HELP		:=	$(STY_ITA)$(STY_WHI)"Run 'make help' to see all available Makefile targets."$(STY_RES)
 
-ifneq (, $(filter $(REBUILD_TARGETS),$(MAKECMDGOALS) $(MODE)))
-MSG_START		:=	$(STY_ITA)"Rebuilding $(NAME) ... "$(STY_RES)
-else
-MSG_START		:=	$(STY_ITA)"Building $(NAME) ... "$(STY_RES)
-endif
+MSG_START		=	$(STY_ITA)$(if $(RECOMPILE),"Rebuilding","Building")" $(NAME) ... "$(STY_RES)
 
 MSG_SUCCESS		?=	$(STY_BOL)$(STY_ITA)$($(COLOR_MAKE))"DONE!"$(STY_RES)
 