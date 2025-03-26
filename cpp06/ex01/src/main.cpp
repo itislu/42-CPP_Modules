adc515573bcd25ff4356074f6ca7f5c75f2c15f0
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-03-26 14:37:44 +0100

    [06/01] refactor: Separate printing into a function

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -4,35 +4,55 @@
 #include "Serializer.hpp"
 #include <cstddef>
 #include <iomanip>
+#include <ios>
 #include <iostream>
 #include <stdint.h>
+#include <string>
+
+template <typename T>
+static void
+print_labeled(const std::string& label,
+              const T& value,
+              std::ios_base::fmtflags fmt = std::ios_base::fmtflags());
 
 int main()
 {
 	// Serializer serializer; // Not instantiable
 	Data data = {"hello world", NULL, 42.42, 100};
 
-	std::cout << std::setw(8) << std::left << "Before:" << &data << '\n';
-	std::cout << std::setw(8) << std::left << "After:"
-	          << Serializer::deserialize(Serializer::serialize(&data)) << '\n';
-	std::cout << std::setw(8) << std::left << "Data:"
-	          << *Serializer::deserialize(Serializer::serialize(&data)) << '\n';
+	print_labeled("Before", &data);
+	print_labeled("After",
+	              Serializer::deserialize(Serializer::serialize(&data)));
+	print_labeled("Data",
+	              *Serializer::deserialize(Serializer::serialize(&data)));
 
 	std::cout << '\n';
 
 	const uintptr_t raw = Serializer::serialize(&data);
 	const Data* ptr = Serializer::deserialize(raw);
 
-	std::cout << std::setw(8) << std::left << "Raw:" << std::setbase(16) << "  "
-	          << raw << std::setbase(10) << '\n';
-	std::cout << std::setw(8) << std::left << "Ptr:" << ptr << '\n';
-	std::cout << std::setw(8) << std::left << "Data:" << *ptr << '\n';
+	print_labeled("Raw", raw, std::ios_base::hex | std::ios_base::showbase);
+	print_labeled("Ptr", ptr);
+	print_labeled("Data", *ptr);
 
 	std::cout << '\n';
 
-	std::cout << std::setw(8) << std::left
-	          << "NULL:" << Serializer::deserialize(Serializer::serialize(NULL))
-	          << '\n';
+	print_labeled("NULL", Serializer::deserialize(Serializer::serialize(NULL)));
+}
+
+template <typename T>
+static void print_labeled(const std::string& label,
+                          const T& value,
+                          std::ios_base::fmtflags fmt)
+{
+	const int label_width = 8;
+	const std::ios_base::fmtflags fmt_backup = std::cout.flags();
+
+	std::cout << std::setw(label_width) << std::left << label + ": ";
+	std::cout.flags(fmt);
+	std::cout << value;
+	std::cout.flags(fmt_backup);
+	std::cout << '\n';
 }
 
 // NOLINTEND(readability-magic-numbers)
