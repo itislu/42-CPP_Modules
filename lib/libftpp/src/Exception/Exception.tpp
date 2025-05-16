itislu/42-CPP_Modules

commit 1a3874891ed9a74e20ef4d27f2354d5c89137293

fix(libftpp): Don't add colors to msg in `Exception`
Exception messages are very likely to be put into a log file.

lib/libftpp/src/Exception/Exception.tpp
@@ -2,7 +2,6 @@
 
 #include "../../Exception.hpp"
 #include "../../Optional.hpp"
-#include "../../format.hpp"
 #include <string>
 
 namespace ft {
@@ -126,8 +125,13 @@ const ft::Optional<std::string>& StdException<BaseException>::where() const
 template <typename BaseException>
 void StdException<BaseException>::_update_what_output()
 {
-	_what_output = (_where ? GRAY(*_where + ": ") : "")
-	               + (_who ? *_who + ": " : "") + _msg;
+	if (_where) {
+		_what_output += *_where + ": ";
+	}
+	if (_who) {
+		_what_output += *_who + ": ";
+	}
+	_what_output += _msg;
 }
 
 template <typename BaseException>

commit b4c836a7e8d3e00ece2cb201559d3c5abf128425

fix(libftpp): Fix updating `StdException` msg appending instead of replacing
The bug was introduced by commit 1a3874891ed9a74e20ef4d27f2354d5c89137293.

Also use `append()` to be more efficient and not construct a temporary string. Most `clear()` implementations for `std::string` keep the capacity.

lib/libftpp/src/Exception/Exception.tpp
@@ -125,13 +125,16 @@ const ft::Optional<std::string>& StdException<BaseException>::where() const
 template <typename BaseException>
 void StdException<BaseException>::_update_what_output()
 {
+	_what_output.clear();
 	if (_where) {
-		_what_output += *_where + ": ";
+		_what_output.append(*_where);
+		_what_output.append(": ");
 	}
 	if (_who) {
-		_what_output += *_who + ": ";
+		_what_output.append(*_who);
+		_what_output.append(": ");
 	}
-	_what_output += _msg;
+	_what_output.append(_msg);
 }
 
 template <typename BaseException>