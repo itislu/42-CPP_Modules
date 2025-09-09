5f510b090e0d2ce5633479924bd1fd4d58ac839e
Lukas Dullinger <itislu.git@gmail.com>
2025-09-09 12:38:49 +0200

    [09/02] chore: Remove `static` from util template

--- cpp09/ex02/src/test_types/busy_wait.hpp
+++ cpp09/ex02/src/test_types/busy_wait.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 template <unsigned long DummyOperations>
-static void busy_wait()
+void busy_wait()
 {
 	if (DummyOperations > 0) {
 		for (volatile unsigned long i = 0; i < DummyOperations / 2; ++i) {
